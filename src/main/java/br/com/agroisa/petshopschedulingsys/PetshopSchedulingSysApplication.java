f121289bdeeecbc62dbe21f360478d4a9511f832
lucaspnhr <lucas_748l@hotmail.com>
2022-10-13 12:05:47 -0300

    Initial commit

---
+++ src/main/java/br/com/agroisa/petshopschedulingsys/PetshopSchedulingSysApplication.java
@@ -0,0 +1,13 @@
+package br.com.agroisa.petshopschedulingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopSchedulingSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetshopSchedulingSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/agroisa/petshopschedulingsys/PetshopSchedulingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agroisa.petshopschedulingsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopSchedulingSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
